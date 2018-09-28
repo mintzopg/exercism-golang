4a9a64d878a77512b00e42760dc2c4663b798a8c
Giorgos Mintzopoulos <gimin@outlook.com>
2018-09-28 20:40:56 +0300

    done

--- go/luhn/valid.go
+++ go/luhn/valid.go
@@ -5,18 +5,17 @@ import (
 	"unicode"
 )
 
+func isValid(r rune) bool {
+	if unicode.IsDigit(r) || unicode.IsSpace(r) {
+		return true
+	}
+	return false
+}
+
 // Valid function(string)bool
 func Valid(s string) bool {
 	ss := []rune(strings.TrimSpace(s))
 	if len(ss) <= 1 {
-		return false
-	}
-
-	// any rune is invalid if not digit or space
-	isValid := func(r rune) bool {
-		if unicode.IsDigit(r) || unicode.IsSpace(r) {
-			return true
-		}
 		return false
 	}
 
