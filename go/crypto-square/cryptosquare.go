56ad21cf00c8149123444e1c9ff8c3c2520e5b23
Giorgos Mintzopoulos <gimin@outlook.com>
2018-10-10 23:21:37 +0300

    working on it

---
+++ go/crypto-square/cryptosquare.go
@@ -0,0 +1,70 @@
+package cryptosquare
+
+import (
+	"math"
+	"strings"
+	"unicode"
+)
+
+// Normalize function(string)string; returns only letters in lowe case & no spaces
+func Normalize(txt string) string {
+	chars := []rune(txt)
+	var sb strings.Builder
+
+	for _, c := range chars {
+		if unicode.IsLetter(c) || unicode.IsDigit(c) {
+			sb.WriteRune(unicode.ToLower(c))
+		}
+	}
+	return sb.String()
+}
+
+func findCol(n int) int {
+	// n number of chars
+	c := math.Sqrt(float64(n))
+	n++
+	for {
+		if c == math.Trunc(c) { // if c is integer return it
+			break
+		} else {
+			n++
+			c = math.Sqrt(float64(n))
+		}
+	}
+	return int(c)
+}
+
+// Encode function(normalized string)string
+func Encode(txt string) string {
+	// normalize the input
+	txt = Normalize(txt)
+	n := len(txt)
+
+	// check the special cases
+	if n == 0 {
+		return ""
+	}
+	if n == 1 {
+		return txt
+	}
+
+	// finc number of rows, columns
+	r := 0
+	c := findCol(n)
+	if c*c == n {
+		r = c
+	} else {
+		r = c - 1
+	}
+
+	chars := []rune(txt)
+	square := make([][]rune, r)
+
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			if c < len(chars) {
+				square[i][j] = append(square[i], chars[j])
+			}
+		}
+	}
+}
