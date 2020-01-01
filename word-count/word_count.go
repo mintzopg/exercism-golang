c6b2f17ade652a46c1bbdac092b5619cbda542b3
Giorgos <gimin@outlook.com>
2020-01-01 13:27:14 +0200

    done

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,1 +1,25 @@
 package wordcount
+
+import (
+	"regexp"
+	"strings"
+)
+
+// Frequency of words appearance in a piece of strings
+type Frequency map[string]int
+
+// WordCount (string) Frequency
+func WordCount(str string) Frequency {
+	freq := make(Frequency, 0)
+
+	// clean the string
+	var re = regexp.MustCompile(`['\w]+`)
+	ss := re.FindAllString(str, -1) // []string
+
+	for _, word := range ss {
+		word = strings.ToLower(word)
+		freq[strings.Trim(word, "'")]++
+	}
+
+	return freq
+}
