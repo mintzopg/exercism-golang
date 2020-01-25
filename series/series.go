963f63ee97316cb36a4e1dabeb1659f5cd7f2343
Giorgos <gimin@outlook.com>
2020-01-25 13:13:35 +0200

    ok

---
+++ series/series.go
@@ -0,0 +1,29 @@
+package series
+
+// All returns a list of all substrings of s with length n
+func All(n int, s string) []string {
+	var out []string
+
+	for i := 0; i < len(s); i++ {
+		if n > len(s)-i {
+			break
+		}
+		out = append(out, s[i:i+n])
+	}
+	return out
+}
+
+// UnsafeFirst returns the first substring of s with length n
+func UnsafeFirst(n int, s string) string {
+	if n > len(s) {
+		return ""
+	}
+	return s[:n]
+}
+
+func First(n int, s string) (first string, ok bool) {
+	if n > len(s) {
+		return "", false
+	}
+	return s[:n], true
+}
