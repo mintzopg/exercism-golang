adfacd801236be8d7a6b0be32cb0a441c54e7885
Giorgos <gimin@outlook.com>
2021-02-26 19:59:00 +0200

    modified

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -6,19 +6,9 @@ import (
 	"time"
 )
 
-// for numbers
-var minNum = 100
-var maxNum = 1000
-
-// for ascii capital letters
-var minASCII = 65 // (A=65)
-var maxASCII = 91 // (Z=90)
-
 var names = map[string]bool{} // map to hold active robot names
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 // Robot structure describes Robot
 type Robot struct {
@@ -31,7 +21,7 @@ func (r *Robot) Name() (string, error) {
 		return r.name, nil
 	}
 	for { // loop will go on until it gets a new valid name
-		r.name = createValidName()       // (1) get a valid random name
+		r.name = newName()               // (1) get a valid random name
 		if _, ok := names[r.name]; !ok { // (2) check if already given
 			names[r.name] = true // (3) if not add it to cache; the value true or false doesn't matter
 			return r.name, nil   // (4) return the string and nil error
@@ -44,16 +34,9 @@ func (r *Robot) Reset() {
 	r.name = ""
 }
 
-/////////////////////////////////////////////////////////////////////////////////
-func randomInt(min, max int) int { // generate random number in range [100, 1000)
-	return rand.Intn(max-min) + min
-}
-
-func createValidName() (name string) {
-	bytes := make([]byte, 2)
-	num := randomInt(minNum, maxNum) // get number in [100, 999]
-	for i := 0; i < 2; i++ {         // get two capital letters
-		bytes[i] = byte(randomInt(minASCII, maxASCII))
-	}
-	return string(bytes) + fmt.Sprintf("%d", num)
+func newName() string {
+	r1 := random.Intn(26) + 'A'
+	r2 := random.Intn(26) + 'A'
+	num := random.Intn(1000)
+	return fmt.Sprintf("%c%c%03d", r1, r2, num)
 }
